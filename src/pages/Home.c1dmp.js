a579270cf4d1c25bcfe44ccaa5832b53946148c3
Amit Kumar <arc.dream@gmail.com>
2024-06-21 14:16:20 +0530

    Update Home.c1dmp.js

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -3,6 +3,6 @@ import { prepareHomeMenuItesm } from 'public/navigation-menu-manager';
 
 
 $w.onReady( function() {
-    console.log("Home Page is Loaded from Win Studio");
+    console.log("Home Page is Loaded from Win Studio - github");
     $w('#topNavigationMenu').menuItems = prepareHomeMenuItesm();
   } );
