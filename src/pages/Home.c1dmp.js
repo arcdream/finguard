ad7b936480fe43dbcc2f34cce27eff5cc3acb506
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-06-21 07:58:25 +0000

    Initial commit

--- src/pages/Home.c1dmp.js
+++ src/pages/Home.c1dmp.js
@@ -3,6 +3,6 @@ import { prepareHomeMenuItesm } from 'public/navigation-menu-manager';
 
 
 $w.onReady( function() {
-    console.log("Home Page is Loaded");
+    console.log("Home Page is Loaded from Win Studio");
     $w('#topNavigationMenu').menuItems = prepareHomeMenuItesm();
   } );
